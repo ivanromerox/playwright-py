dacf2bafbf9b8d18636987b5fe7b0ccf7d664e66
Ivan Romero <ivanche.alejo@gmail.com>
2026-01-08 09:20:53 -0300

    Models for type safety added

---
+++ models/__init__.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import List, Optional, Union
+
+
+@dataclass
+class AirWaybill:
+	code: str
+	destination: str
+	weight: Union[int, float]
+
+
+@dataclass
+class Invoice:
+	number: str
+	issue_date: str
+	service_month: str
+	amount: Union[int, float]
+	attachment: str
+	details: Optional[str]
+	air_waybills: Optional[List[AirWaybill]]
+	items: Optional[int]
+	comment: str
